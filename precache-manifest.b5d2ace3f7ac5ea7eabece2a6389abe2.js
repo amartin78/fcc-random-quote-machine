2cdfbe95534d09064e57b268b17ee92b125e1c95
antonio <ammj24@hotmail.com>
2019-07-05 23:25:36 +0100

    Updates

---
+++ precache-manifest.b5d2ace3f7ac5ea7eabece2a6389abe2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0feb79e8370f35a50f2966aef239d3f",
+    "url": "/fcc-random-quote-machine/index.html"
+  },
+  {
+    "revision": "bc3cd1cb6d93ffa111b6",
+    "url": "/fcc-random-quote-machine/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/fcc-random-quote-machine/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "bc3cd1cb6d93ffa111b6",
+    "url": "/fcc-random-quote-machine/static/js/main.d2ab9349.chunk.js"
+  },
+  {
+    "revision": "41426a8a6f6aff379df0",
+    "url": "/fcc-random-quote-machine/static/js/runtime~main.10b46977.js"
+  }
+]);
