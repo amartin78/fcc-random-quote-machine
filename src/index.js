8ed544aa9730f36e43da16bb6ea37dc7461927f8
antonio <ammj24@hotmail.com>
2019-07-05 20:39:52 +0100

    build prod

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-ReactDOM.render(<p>Random Quote Machine</p>, document.getElementById('root'));
+ReactDOM.render(<p>Random Quote Machineg</p>, document.getElementById('root'));
 
 
 
