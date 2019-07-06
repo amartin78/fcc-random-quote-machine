89a91b6d13e700290d2576865ee8bfa202e77582
antonio <ammj24@hotmail.com>
2019-07-06 01:19:02 +0100

    fetch quote

--- src/index.js
+++ src/index.js
@@ -2,7 +2,51 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-ReactDOM.render(<p>Random Quote Machineg</p>, document.getElementById('root'));
 
+
+class Main extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            isLoaded: false,
+            text : '',
+            author: '',
+            error: ''
+        }
+    }
+
+    componentDidMount() {
+        fetch("https://cors-anywhere.herokuapp.com/api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en")
+        .then(res => res.json())
+        .then(
+            (result) => {
+                this.setState({
+                    isLoaded: true,
+                    text: result.quoteText,
+                    author: result.quoteAuthor,
+                });
+            },
+            (error) => {
+                this.setState({
+                    isLoaded: true,
+                    error
+                });
+            }
+        )
+    }
+
+    render() {
+        return (
+            <div id="quote-box">
+                <div id="text">{this.state.text + " - " + this.state.author}</div>
+            </div>
+        );
+    }
+}
+
+
+
+ReactDOM.render(<Main />, document.getElementById('root'));
 
 
