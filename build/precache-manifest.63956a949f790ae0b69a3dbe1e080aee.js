6917f52e5eebd78b5d2eb9d0c6e35e3a8117c2b4
antonio <ammj24@hotmail.com>
2019-07-12 19:07:28 +0100

    build

---
+++ build/precache-manifest.63956a949f790ae0b69a3dbe1e080aee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36a97a74c4b1737d34f20d2b5d8b36d7",
+    "url": "/fcc-random-quote-machine/index.html"
+  },
+  {
+    "revision": "663161464fc895481ce7",
+    "url": "/fcc-random-quote-machine/static/css/main.9d98308f.chunk.css"
+  },
+  {
+    "revision": "58d0316c837413e4e4fd",
+    "url": "/fcc-random-quote-machine/static/js/2.e60f0bc4.chunk.js"
+  },
+  {
+    "revision": "663161464fc895481ce7",
+    "url": "/fcc-random-quote-machine/static/js/main.7e599351.chunk.js"
+  },
+  {
+    "revision": "41426a8a6f6aff379df0",
+    "url": "/fcc-random-quote-machine/static/js/runtime~main.10b46977.js"
+  }
+]);
